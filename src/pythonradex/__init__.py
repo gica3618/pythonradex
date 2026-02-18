7155e6fa1dbac0c858b4576490bf5b23b386f565
gica3618 <cataldi.gia@gmail.com>
2026-02-18 10:30:16 +0900

    added __version__ to __init__.py

--- src/pythonradex/__init__.py
+++ src/pythonradex/__init__.py
@@ -4,3 +4,6 @@ Python implementation of the RADEX radiative transfer code
 """
 
 from . import helpers, LAMDA_file, radiative_transfer
+from importlib.metadata import version
+
+__version__ = version("pythonradex")
